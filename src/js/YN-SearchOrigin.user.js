83ff6053faf3298d0e810ca6d60d9150dfe7e1ee
Masao S <masawo@gmail.com>
2024-09-27 17:29:35 +0900

    ページ構造変更に伴う修正

--- src/js/YN-SearchOrigin.user.js
+++ src/js/YN-SearchOrigin.user.js
@@ -3,7 +3,7 @@
 // @name:ja         Yahoo!ニュースの元記事を探す
 // @namespace       https://furyutei.work
 // @license         MIT
-// @version         0.1.16
+// @version         0.1.17
 // @description     Find the original article of the article of Yahoo News Japan.
 // @description:ja  Yahoo!ニュースの記事の、元となった記事探しを助けます
 // @author          furyu
@@ -407,7 +407,7 @@ const
     
     get_search_info = () => {
         const
-            site_link = document.querySelector( 'main[id="contents"] div[id="contentsWrap"] > article > header > div > div:last-child > a' );
+            site_link = document.querySelector( 'main[id="contents"] div[id="contentsWrap"] > article > header > div > a' );
         
         if ( ! site_link ) {
             return null;
